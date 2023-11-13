e40567c48512e9b0dd56713ee5fa269f8ab5bd56
Amirhossein Daghigh Kia <amirhosseindk@hotmail.com>
2023-11-13 23:29:12 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static int SansaXor(List<int> numbersArray)
+{
+	if (numbersArray.Count % 2 == 0)
+	{
+		return 0;
+	}
+
+	int result = 0;		
+
+	for (int index = 0; index < numbersArray.Count; index += 2)
+	{
+		result ^= numbersArray[index];
+	}
+
+	return result;
+}
